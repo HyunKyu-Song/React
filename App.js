5a38f04c23592e5a4f1bdb1bb285e9a4aa4cb52c
송현규 <108565747+HyunKyu-Song@users.noreply.github.com>
2022-10-20 22:25:20 +0900

    React

--- App.js
+++ App.js
@@ -2,62 +2,90 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
-
 function App() {
-
   let [아이돌, 팀명변경] = useState(['프로미스나인', '르세라핌', '아이브']);
-  let [좋아요, 좋아요변경] = useState(0);
+  let [좋아요, 좋아요변경] = useState([0, 0, 0]);
   let [모달, 모달변경] = useState(false);
+  let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState('');
 
   return (
     <div className="App">
       <div className='black-nav'>
         <h4 style={{ color: 'coral' }}>아이돌 Blog</h4>
       </div>
-      <div className='list'>
-        <h4>{아이돌[0]} <span onClick={() => {
-          좋아요변경(좋아요 + 1);
-        }}>❤</span> {좋아요} </h4>
-        <p>10월 18일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{아이돌[1]}</h4>
-        <p>10월 18일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{아이돌[2]}</h4>
-        <p>10월 18일 발행</p>
-      </div>
+
+      {
+        아이돌.map(function (i, j) {
+          return (
+            <div className='list' key={i}>
+              <h4 onClick={()=>{
+                모달변경(!모달);
+                setTitle(j);
+              }}>{i} <span onClick={(e) => {
+                e.stopPropagation();
+                let copy = [...좋아요];
+                copy[j] += 1;
+                좋아요변경(copy);
+              }}>❤</span> {좋아요[j]} <button onClick={(e)=>{
+                e.stopPropagation();
+                let copy = [...아이돌];
+                copy.splice(j, 1);
+                팀명변경(copy);
+              }}>삭제</button> </h4>
+              <p>10월 18일 발행</p>
+            </div>
+          )
+        })
+      }
 
       <button onClick={() => {
         let copy = [...아이돌];
         copy[0] = 'fromis_9';
         팀명변경(copy);
       }}>버튼</button>
-      <br/>
+      <br />
 
       <button onClick={() => {
         let copy = [...아이돌];
         팀명변경(copy.sort());
       }}>오름차순</button>
-      <br/>
+      <br />
 
       <button onClick={() => {
         모달변경(!모달);
       }}>Modal</button>
 
-      { 모달 == true ? <Modal></Modal> : null }
+      {모달 == true ? <Modal color = "coral" title = { title } 아이돌 = { 아이돌 } 팀명변경 = { 팀명변경 }></Modal> : null}
+      <br/>
+
+      <input onChange={(e)=>{
+        입력값변경(e.target.value);
+        console.log(입력값);
+      }}></input>
+      <button onClick={()=>{
+        if(입력값 != ''){
+          let copy = [...아이돌];
+          copy.push(입력값);
+          팀명변경(copy);
+        }
+      }}>버튼</button>
 
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
-    <div className='modal'>
-      <h4>제목</h4>
+    <div className='modal' style={{background : props.color}}>
+      <h4>{ props.아이돌[props.title] }</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={()=>{
+        let copy = [...props.아이돌];
+        copy[0] = 'fromis_9';
+        props.팀명변경(copy);
+      }}>버튼</button>
     </div>
   )
 }
