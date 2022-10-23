f055202db54d5fc6a48e70ab2e742820fb2920aa
송현규 <108565747+HyunKyu-Song@users.noreply.github.com>
2022-10-23 22:04:54 +0900

    React

--- App.js
+++ App.js
@@ -2,29 +2,26 @@ import { useState } from 'react';
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import './App.css';
 import data from './data.js';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './detail.js';
 
 function App() {
 
-  let [shoes] = useState(data);
+  let [shoes, shoesSet] = useState(data);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
-
-
-
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand>
             <Link className='link' to="/">아이돌 굿즈샵</Link>
           </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link>
-              <Link className='link' to="/detail">상세페이지</Link>
-            </Nav.Link>
-            <Nav.Link href="#home">fromis_9</Nav.Link>
-            <Nav.Link href="#features">르세라핌</Nav.Link>
-            <Nav.Link href="#pricing">아이브</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/detail') }}>상세페이지</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/fromis') }}>프로미스나인</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/lesserafim') }}>르세라핌</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/ive') }}>아이브</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -46,12 +43,35 @@ function App() {
                 }
               </Row>
             </Container>
+            <button onClick={() => {
+              let copy = [...shoes];
+              copy = copy.sort((a, b) => {
+                if (a.title > b.title)
+                  return 1;
+                if (a.title < b.title)
+                  return -1;
+                return 0;
+              });
+              shoesSet(copy);
+            }}>가나다순</button>
           </>
         } />
-        <Route path="/detail" element={<div>상세페이지임</div>} />
+        <Route path="/detail" element={<Detail shoes={shoes}></Detail>} />
+        <Route path="/detail/:id" element={<Detail shoes={shoes}></Detail>} />
+        <Route path="/fromis" element={<div>프로미스나인</div>} />
+        <Route path="/lesserafim" element={<div>르세라핌</div>} />
+        <Route path="/ive" element={<div>아이브</div>} />
+        <Route path="*" element={<div>없는 페이지입니다.</div>} />
+
+        {/* <Route path="/idol" element={<div>아이돌 <Outlet></Outlet></div>} >
+          <Route path="fromis" element={<div>프로미스나인</div>} />
+          <Route path="lesserafim" element={<div>르세라핌</div>} />
+          <Route path="ive" element={<div>아이브</div>} />
+        </Route> */}
+
       </Routes>
 
-    </div>
+    </div >
   );
 }
 
